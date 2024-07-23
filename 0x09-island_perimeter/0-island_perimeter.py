5e8e51ff40046daa95aadef5371a185d33677952
emaoumaima <sellouane.oumaima@gmail.com>
2024-07-23 00:52:57 +0000

    Update file

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,43 +1,26 @@
 #!/usr/bin/python3
-"""Island perimeter coding challenge
-"""
+"""Defines island perimeter finding function."""
 
 
 def island_perimeter(grid):
-    """Determine the perimeter of an island made up of square pieces of land
-    Only one island; no lakes; if data is invalid, return 0.
+    """Return the perimiter of an island.
+    The grid represents water by 0 and land by 1.
+    Args:
+        grid (list): A list of list of integers representing an island.
+    Returns:
+        The perimeter of the island defined in grid.
     """
-    # Check for edge cases:
-    try:
-        assert grid and type(grid) == list
-        for row in grid:
-            assert type(row) == list
-            for cell in row:
-                assert cell == 0 or cell == 1
-    except Exception:
-        #  raise
-        return 0
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
 
-    # Establish grid dimensions
-    rows = len(grid)
-    cols = len(grid[0])
-
-    # Initialize the perimeter
-    result = 0
-
-    for i in range(rows):
-        for j in range(cols):
+    for i in range(height):
+        for j in range(width):
             if grid[i][j] == 1:
-                # Inspect the 4 sides
-                for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-                    i2, j2 = i + dx, j + dy
-                    # Check neighbour, add 1 if necessary
-                    if i2 < 0 or i2 >= rows or j2 < 0 or \
-                            j2 >= cols or grid[i2][j2] == 0:
-                        result += 1
-
-    return result
-
-
-if __name__ == "__main__":
-    pass
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
